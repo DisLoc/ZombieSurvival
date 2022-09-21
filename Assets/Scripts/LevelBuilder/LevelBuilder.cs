1d9085fbb9a3acc92ed1839a99273aaa931dc626
MattewGame <smelov.mathews@yandex.ru>
2022-09-21 18:50:26 +0300

    Add survival time

--- Assets/Scripts/LevelBuilder/LevelBuilder.cs
+++ Assets/Scripts/LevelBuilder/LevelBuilder.cs
@@ -9,10 +9,24 @@ public class LevelBuilder : MonoBehaviour
 
     [Header("Settings")]
     [SerializeField] private GameObject _groundPrefab;
+    
+
+    [Header("Mobs")]
+    [SerializeField] private Transform _player;
+
+    private Vector3 _startPlayerPosition;
 
     private void OnEnable()
     {
-        
+        _startPlayerPosition = _player.position;
+    }
+
+    private void Update()
+    {
+        if((_startPlayerPosition.x - _player.position.x) / -3.3f == 1)
+        {
+            Instantiate(_groundPrefab, _player.position, Quaternion.identity);
+        }
     }
 
     private void OnDisable()
---
+++ Assets/Scripts/PlayerControl/SurvivalTime.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SurvivalTime : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField] private UIController _uIController;
+
+    private Text _survivalTimeCountText;
+
+    private int _survivalTimeCount;
+    void Start()
+    {
+        _survivalTimeCountText = _uIController.SurvivalTimeCountText;
+        StartCoroutine(PlusTimeCount());
+    }
+
+    private IEnumerator PlusTimeCount()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+            _survivalTimeCount++;
+            _survivalTimeCountText.text = _survivalTimeCount.ToString();
+        }
+    }
+}
