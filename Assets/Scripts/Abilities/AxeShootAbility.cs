0046ae29ec8db2aa4265e849e92853734a9730b1
MattewGame <smelov.mathews@yandex.ru>
2022-09-30 15:33:19 +0300

    fix active ability

--- Assets/Scripts/Abilities/AxeShootAbility.cs
+++ Assets/Scripts/Abilities/AxeShootAbility.cs
@@ -4,14 +4,35 @@ using UnityEngine;
 
 public class AxeShootAbility : MonoBehaviour
 {
-
+    
     [SerializeField] protected GameObject projectile;
 
     [SerializeField] private Transform shootPosition;
+
+    private int _coolDown;
+
+    private void Start()
+    {
+        StartCoroutine(Shoot());
+    }
 
 
     public void UseState()
     {
         Instantiate(projectile, shootPosition.position, Quaternion.identity);
+    }
+
+    private IEnumerator Shoot()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(_coolDown);
+            UseState();
+        }
+    }
+
+    public void SetCoolDown(int coolDown)
+    {
+        _coolDown = coolDown;
     }
 }
