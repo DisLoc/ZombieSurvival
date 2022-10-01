0daef0a1c3f0acbc69ba13cbb4e7bc658e5c9f92
Pavel Polozov <ppolozov.322@gmail.com>
2022-10-01 18:54:12 +0500

    LevelBuilder fix

--- Assets/Scripts/LevelBuilder/LevelBuilder.cs
+++ Assets/Scripts/LevelBuilder/LevelBuilder.cs
@@ -1,8 +1,6 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Zenject;
-using static UnityEditor.PlayerSettings;
 
 public class LevelBuilder : MonoBehaviour
 {
@@ -21,90 +19,63 @@ public class LevelBuilder : MonoBehaviour
 
     private GridXZ _grid;
     private GroundGrid _groundGrid;
-    private List<Cell> _cells;
     private int _maxDeltaIndex;
+
+    private Dictionary<Vector2, Cell> _cells;
 
     [ContextMenu("Init")]
     public void Init()
     {
         _grid = new GridXZ(groundGrid, this);
-        _cells = new List<Cell>();
+        _cells = new Dictionary<Vector2, Cell>();
         _groundGrid = groundGrid;
 
         _maxDeltaIndex = _visionRange / _groundGrid.CellSize;
-        
-        for (int i = -_maxDeltaIndex; i <= _maxDeltaIndex; i++)
-            for (int j = -_maxDeltaIndex; j <= _maxDeltaIndex; j++)
-            {
-                Vector3 pos = new Vector3
-                    (
-                        groundGrid.CellSize * i * 2,
-                        _gridHeight,
-                        groundGrid.CellSize * j * 2
-                    );
 
-                Cell cell = Instantiate(_grid.GetCell(i, j), pos, Quaternion.identity,  transform);
-                cell.Initialize(i, j, _grid);
+        Vector3 pos = new Vector3(0, _gridHeight, 0);
 
-                _cells.Add(cell);
-            }
+        Cell cell = Instantiate(_grid.GetCell(0, 0), pos, Quaternion.identity, transform);
+        cell.Initialize(0, 0, _grid);
+
+        _cells.Add(new Vector2(0, 0), cell);
+
+        UpdateGrid(cell);
     }
 
     [Inject]
     public void Construct(GroundGrid grid)
     {
-        _grid = new GridXZ(grid, this);
-        _cells = new List<Cell>();
-        _groundGrid = grid;
+        _grid = new GridXZ(groundGrid, this);
+        _cells = new Dictionary<Vector2, Cell>();
+        _groundGrid = groundGrid;
 
         _maxDeltaIndex = _visionRange / _groundGrid.CellSize;
 
-        for (int i = -_maxDeltaIndex + 1; i < _maxDeltaIndex - 1; i++)
-            for (int j = -_maxDeltaIndex + 1; j < _maxDeltaIndex - 1; j++)
-            {
-                Vector3 pos = new Vector3
-                    (
-                        groundGrid.CellSize * i * 2,
-                        _gridHeight,
-                        groundGrid.CellSize * j * 2
-                    );
+        Vector3 pos = new Vector3(0, _gridHeight, 0);
 
-                Cell cell = Instantiate(_grid.GetCell(i, j), pos, Quaternion.identity, transform);
-                cell.Initialize(i, j, _grid);
+        Cell cell = Instantiate(_grid.GetCell(0, 0), pos, Quaternion.identity, transform);
+        cell.Initialize(0, 0, _grid);
 
-                _cells.Add(cell);
-            }
+        _cells.Add(new Vector2(0, 0), cell);
+
+        UpdateGrid(cell);
     }
 
     public void UpdateGrid(Cell enterCell)
     {
         if (_isDebug) Debug.Log("Update grid");
 
-        int maxX = 0, minX = 0, maxZ = 0, minZ = 0;
-        
-        foreach (Cell cell in _cells)
+        for(int i = enterCell.X - _maxDeltaIndex; i <= enterCell.X + _maxDeltaIndex; i++)
         {
-            if (cell.X < minX) minX = cell.X;
-            if (cell.X > maxX) maxX = cell.X;
-            if (cell.Z < minZ) minZ = cell.Z;
-            if (cell.Z > maxZ) maxZ = cell.Z;
+            for (int j = enterCell.Z - _maxDeltaIndex; j <= enterCell.Z + _maxDeltaIndex; j++)
+            {
+                Vector2 index = new Vector2(i, j);
 
-            if (cell.X < enterCell.X - _maxDeltaIndex || cell.X > enterCell.X + _maxDeltaIndex ||
-                cell.Z < enterCell.Z - _maxDeltaIndex || cell.Z > enterCell.Z + _maxDeltaIndex)
-            {
-                cell.gameObject.SetActive(false);
-            }
-            else
-            {
-                cell.gameObject.SetActive(true);
-            }
-        }
-
-        if (maxX < enterCell.X + _maxDeltaIndex)
-        {
-            for (int i = maxX + 1; i <= enterCell.X + _maxDeltaIndex; i++)
-            {
-                for (int j = enterCell.Z - _maxDeltaIndex; j <= enterCell.Z + _maxDeltaIndex; j++)
+                if (_cells.ContainsKey(index))
+                {
+                    continue;
+                }
+                else
                 {
                     Vector3 pos = new Vector3
                     (
@@ -116,74 +87,9 @@ public class LevelBuilder : MonoBehaviour
                     Cell cell = Instantiate(_grid.GetCell(i, j), pos, Quaternion.identity, transform);
                     cell.Initialize(i, j, _grid);
 
-                    _cells.Add(cell);
+                    _cells.Add(index, cell);
                 }
             }
         }
-        if (minX > enterCell.X - _maxDeltaIndex)
-        {
-            for (int i = enterCell.X - _maxDeltaIndex; i < minX; i++)
-            {
-                for (int j = enterCell.Z - _maxDeltaIndex; j <= enterCell.Z + _maxDeltaIndex; j++)
-                {
-                    Vector3 pos = new Vector3
-                    (
-                        groundGrid.CellSize * i * 2,
-                        _gridHeight,
-                        groundGrid.CellSize * j * 2
-                    );
-
-                    Cell cell = Instantiate(_grid.GetCell(i, j), pos, Quaternion.identity, transform);
-                    cell.Initialize(i, j, _grid);
-
-                    _cells.Add(cell);
-                }
-            }
-        }
-        if (maxZ < enterCell.Z + _maxDeltaIndex)
-        {
-            for (int i = enterCell.X - _maxDeltaIndex; i <= enterCell.X + _maxDeltaIndex; i++)
-            {
-                for (int j = maxZ + 1; j <= enterCell.Z + _maxDeltaIndex; j++)
-                {
-                    Vector3 pos = new Vector3
-                    (
-                        groundGrid.CellSize * i * 2,
-                        _gridHeight,
-                        groundGrid.CellSize * j * 2
-                    );
-
-                    Cell cell = Instantiate(_grid.GetCell(i, j), pos, Quaternion.identity, transform);
-                    cell.Initialize(i, j, _grid);
-
-                    _cells.Add(cell);
-                }
-            }
-        }
-        if (minZ > enterCell.Z - _maxDeltaIndex)
-        {
-            for (int i = enterCell.X - _maxDeltaIndex; i <= enterCell.X + _maxDeltaIndex; i++)
-            {
-                for (int j = enterCell.Z - _maxDeltaIndex; j < minZ; j++)
-                {
-                    Vector3 pos = new Vector3
-                    (
-                        groundGrid.CellSize * i * 2,
-                        _gridHeight,
-                        groundGrid.CellSize * j * 2
-                    );
-
-                    Cell cell = Instantiate(_grid.GetCell(i, j), pos, Quaternion.identity, transform);
-                    cell.Initialize(i, j, _grid);
-
-                    _cells.Add(cell);
-                }
-            }
-        }
-    }
-
-    public void Create()
-    {
-
     }
 }
