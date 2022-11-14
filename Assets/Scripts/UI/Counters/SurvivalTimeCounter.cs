84ffc96f21df0a6fd614505cd56658e6cb3b35cd
Pavel Polozov <ppolozov.322@gmail.com>
2022-11-14 15:27:32 +0500

    Boss event fix
    Now timer & level progress stops on boss event
    
    + chest spawn interval fix

--- Assets/Scripts/UI/Counters/SurvivalTimeCounter.cs
+++ Assets/Scripts/UI/Counters/SurvivalTimeCounter.cs
@@ -1,24 +1,30 @@
-using System;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SurvivalTimeCounter : MonoBehaviour, IGameStartHandler
+public class SurvivalTimeCounter : MonoBehaviour, IGameStartHandler, IBossEventHandler, IBossEventEndedHandler
 {
     [SerializeField] private LevelProgress _levelProgress;
     [SerializeField] private Text _survivalTimeText;
 
-    private float _survivalTime;
+    private float _survivalTime; 
+    private bool _onBossEvent;
 
     public float SurvivalTime => _survivalTime;
 
-    public void OnGameStart()
+    private void OnEnable()
     {
-        _survivalTime = 0f;
-        UpdateTimerText();
+        EventBus.Subscribe(this);
+    }
+
+    private void OnDisable()
+    {
+        EventBus.Unsubscribe(this);
     }
 
     private void FixedUpdate()
     {
+        if (_onBossEvent) return;
+
         _survivalTime += Time.fixedDeltaTime;
 
         _levelProgress.OnTimerUpdate();
@@ -29,5 +35,21 @@ public class SurvivalTimeCounter : MonoBehaviour, IGameStartHandler
     private void UpdateTimerText()
     {
         _survivalTimeText.text = IntegerFormatter.GetTime((int)_survivalTime);
+    }
+
+    public void OnGameStart()
+    {
+        _survivalTime = 0f;
+        UpdateTimerText();
+    }
+
+    public void OnBossEvent()
+    {
+        _onBossEvent = true;
+    }
+
+    public void OnBossEventEnd()
+    {
+        _onBossEvent = false;
     }
 }
